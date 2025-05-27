81b8b29a09b32a5c51ab436c94830293445e87f2
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-27 17:00:48 +0700

    add validator in text field

--- lib/core/components/custom_text_field.dart
+++ lib/core/components/custom_text_field.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'spaces.dart';
 
 class CustomTextField extends StatelessWidget {
+  final String validator;
   final TextEditingController controller;
   final String label;
   final Function(String value)? onChanged;
@@ -26,6 +27,7 @@ class CustomTextField extends StatelessWidget {
     this.suffixIcon,
     this.readOnly = false,
     this.maxLines = 1,
+    required this.validator,
   });
 
   @override
@@ -44,6 +46,13 @@ class CustomTextField extends StatelessWidget {
           const SpaceHeight(12.0),
         ],
         TextFormField(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (vl) {
+            if (vl == null || vl.isEmpty) {
+              return validator;
+            }
+            return null;
+          },
           controller: controller,
           onChanged: onChanged,
           obscureText: obscureText,
