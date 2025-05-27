1f71fae44795b822b3c4c57fdc46834800be9f6d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-27 18:51:03 +0700

    create business logic for login

---
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:bloc/bloc.dart';
+import 'package:canary_farm/data/models/request/auth/login_request_model.dart';
+import 'package:canary_farm/data/models/response/auth_response_model.dart';
+import 'package:canary_farm/data/repository/auth_repository.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState> emit,
+  ) async {
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l)),
+      (r) => emit(LoginSuccess(responseModel: r)),
+    );
+  }
+}
