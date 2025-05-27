658d56ecbedfd1ebc8fbf55849e7f4828f74681d
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-27 17:49:01 +0700

    create http client for hitting endpoint

---
+++ lib/services/service_http_client.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class ServiceHttpClient {
+  final String baseUrl = 'http://10.21.31.122:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endPoint");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+
+      _logRequest("POST", url, body, response);
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  Future<http.Response> get(String endPoint) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+    try {
+      final response = await http.get(
+        url,
+        headers: {
+          if (token != null)
+            'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      _logRequest("GET", url, null, response);
+      return response;
+    } catch (e) {
+      throw Exception("GET request failed: $e");
+    }
+  }
+
+  Future<http.Response> postWihToken(
+    String endPoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endPoint");
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+
+      _logRequest("POST", url, body, response);
+      return response;
+    } catch (e) {
+      throw Exception("POST request failed: $e");
+    }
+  }
+
+  void _logRequest(
+    String method,
+    Uri url,
+    Map<String, dynamic>? body,
+    http.Response response,
+  ) {
+    print("==== HTTP REQUEST ====");
+    print("Method: $method");
+    print("URL: $url");
+    if (body != null) print("Body: ${jsonEncode(body)}");
+    print("Status Code: ${response.statusCode}");
+    print("Response Body: ${response.body}");
+    print("======================");
+  }
+}
