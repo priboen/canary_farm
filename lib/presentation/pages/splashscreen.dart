b728ec6d19928bd1ba85fc498cb7fecdb0dd3687
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-29 12:11:23 +0700

    create splashscreen for checking local storage

---
+++ lib/presentation/pages/splashscreen.dart
@@ -0,0 +1,52 @@
+import 'package:canary_farm/core/core.dart';
+import 'package:canary_farm/presentation/admin/admin_main_page.dart';
+import 'package:canary_farm/presentation/auth/login_screen.dart';
+import 'package:canary_farm/presentation/buyer/home/buyer_home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  final _storage = const FlutterSecureStorage();
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () async {
+      final token = await await _storage.read(key: 'authToken');
+      if (token != null) {
+        final role = await _storage.read(key: 'userRole');
+        if (role?.toLowerCase() == 'admin') {
+          context.pushAndRemoveUntil(const AdminMainPage(), (route) => false);
+        } else {
+          context.pushAndRemoveUntil(const BuyerHomeScreen(), (route) => false);
+        }
+      } else {
+        context.pushAndRemoveUntil(
+          const LoginScreen(),
+          (route) => false,
+        );
+      }
+    });
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Canary Farm',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
