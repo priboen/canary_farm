e84d51744e2729bc5aef5075bf57d931041cc89a
IndraAlFauzan <nurulindra35@gmail.com>
2025-05-29 21:03:29 +0700

    make model request

---
+++ lib/data/models/request/admin/anak_request_model.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+class AnakRequestModel {
+  final String noRing;
+  final DateTime tanggalLahir;
+  final String jenisKelamin;
+  final String jenisKenari;
+  final String keterangan;
+  final String gambarBurung;
+  final int ayahId;
+  final int ibuId;
+
+  AnakRequestModel({
+    required this.noRing,
+    required this.tanggalLahir,
+    required this.jenisKelamin,
+    required this.jenisKenari,
+    required this.keterangan,
+    required this.gambarBurung,
+    required this.ayahId,
+    required this.ibuId,
+  });
+
+  AnakRequestModel copyWith({
+    String? noRing,
+    DateTime? tanggalLahir,
+    String? jenisKelamin,
+    String? jenisKenari,
+    String? keterangan,
+    String? gambarBurung,
+    int? ayahId,
+    int? ibuId,
+  }) => AnakRequestModel(
+    noRing: noRing ?? this.noRing,
+    tanggalLahir: tanggalLahir ?? this.tanggalLahir,
+    jenisKelamin: jenisKelamin ?? this.jenisKelamin,
+    jenisKenari: jenisKenari ?? this.jenisKenari,
+    keterangan: keterangan ?? this.keterangan,
+    gambarBurung: gambarBurung ?? this.gambarBurung,
+    ayahId: ayahId ?? this.ayahId,
+    ibuId: ibuId ?? this.ibuId,
+  );
+
+  factory AnakRequestModel.fromRawJson(String str) =>
+      AnakRequestModel.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory AnakRequestModel.fromJson(Map<String, dynamic> json) =>
+      AnakRequestModel(
+        noRing: json["no_ring"],
+        tanggalLahir: DateTime.parse(json["tanggal_lahir"]),
+        jenisKelamin: json["jenis_kelamin"],
+        jenisKenari: json["jenis_kenari"],
+        keterangan: json["keterangan"],
+        gambarBurung: json["gambar_burung"],
+        ayahId: json["ayah-id"],
+        ibuId: json["ibu_id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+    "no_ring": noRing,
+    "tanggal_lahir":
+        "${tanggalLahir.year.toString().padLeft(4, '0')}-${tanggalLahir.month.toString().padLeft(2, '0')}-${tanggalLahir.day.toString().padLeft(2, '0')}",
+    "jenis_kelamin": jenisKelamin,
+    "jenis_kenari": jenisKenari,
+    "keterangan": keterangan,
+    "gambar_burung": gambarBurung,
+    "ayah-id": ayahId,
+    "ibu_id": ibuId,
+  };
+}
