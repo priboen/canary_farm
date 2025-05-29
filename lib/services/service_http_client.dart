6d82ccd4fed6cb1fb65c097c6806de85d68d6838
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-05-29 12:10:57 +0700

    change to localhost emulator IP

--- lib/services/service_http_client.dart
+++ lib/services/service_http_client.dart
@@ -4,7 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceHttpClient {
-  final String baseUrl = 'http://10.21.31.122:8000/api/';
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
   final secureStorage = FlutterSecureStorage();
 
   Future<http.Response> post(String endPoint, Map<String, dynamic> body) async {
